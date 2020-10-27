3d62d4cfe3c695f54f824a4f7436a8b8740f4e79
ExpectozJJ <32187437+ExpectozJJ@users.noreply.github.com>
2020-10-27 13:05:26 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
-__version__ = "0.2"
-__author__ = "Wee JunJie"
-__email__ = "WEEJ0016@e.ntu.edu.sg"
-
-
-name = "GeneralisedFormanRicci"
-
-from .GeneralisedFormanRicci import GeneralisedFormanRicci
+from GeneralisedFormanRicci.frc import GeneralisedFormanRicci
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GeneralisedFormanRicci", # Replace with your own username
+    version="0.0.2",
+    author="Wee JunJie",
+    author_email="expectozjj@gmail.com",
+    description="A class to compute the Generalised Forman-Ricci curvature for a Simplicial Complex from a given point cloud data.",
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    url="https://github.com/ExpectozJJ/GeneralisedFormanRicci",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
