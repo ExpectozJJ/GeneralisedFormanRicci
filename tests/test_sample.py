4f61894ffb7e8b9c32a855fa7f8dde6a5fbe9a23
ExpectozJJ <32187437+ExpectozJJ@users.noreply.github.com>
2020-10-27 18:08:10 +0800

    Delete test_sample.py

--- tests/test_sample.py
+++
@@ -1,7 +0,0 @@
-from GeneralisedFormanRicci.frc import GeneralisedFormanRicci
-
-def test_sample():
-  data = [[0.8, 2.6], [0.2, 1.0], [0.9, 0.5], [2.7, 1.8], [1.7, 0.5], [2.5, 2.5], [2.4, 1.0], [0.6, 0.9], [0.4, 2.2]]
-  for f in [0, 0.5, 1, 2, 3]:
-      sc = GeneralisedFormanRicci(data, method = "rips", epsilon = f)
-      sc.compute_forman()
